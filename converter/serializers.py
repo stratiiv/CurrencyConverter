10cc20dcd2ebc33c12abeb61e177027d9467bb4a
stratiiv <stratiyvalter@gmail.com>
2023-06-03 18:46:49 +0300

    add serializer validation

--- converter/serializers.py
+++ converter/serializers.py
@@ -1,10 +1,25 @@
 from rest_framework import serializers
 
 
+ALLOWED_CURRENCIES = ("USD", "EUR", "GBP", "UAH")
+
+
 class ConversionSerializer(serializers.Serializer):
     """Serializes conversion request"""
     from_currency = serializers.CharField(max_length=3)
     to_currency = serializers.CharField(max_length=3)
-    value = serializers.DecimalField(max_digits=20, decimal_places=10)
+    amount = serializers.DecimalField(max_digits=20, decimal_places=10)
 
-    
+    def validate(self, data):
+        """Check if currency is in allowed list"""
+        check_list = data["from_currency"], data["to_currency"]
+        if not any(currency in ALLOWED_CURRENCIES for currency in check_list):
+            raise serializers.ValidationError(f"Please choose currency from {ALLOWED_CURRENCIES}")
+        return data
+
+    def validate_amount(self, value):
+        """Check if currency is positive number"""
+        if value <= 0:
+            raise serializers.ValidationError("Amount must be positive number")
+        return value
+            
