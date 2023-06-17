5145408fd93ad9d8639c8f5e233f7b2f2b7e96a1
stratiiv <stratiyvalter@gmail.com>
2023-06-17 22:02:44 +0300

    add tests

--- converter/tests.py
+++ converter/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+import pytest
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.urls import reverse
 
-# Create your tests here.
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+
+@pytest.fixture
+def conversion_url():
+    return reverse('convert')
+
+
+def test_conversion_sucessful(api_client, conversion_url):
+    data = {'from': 'USD', 'to': 'UAH', 'amount': 100}
+    response = api_client.get(conversion_url, data)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data['currency'] == 'UAH'
+    assert 'value' in response.data
+    assert 'exchange_rate' in response.data
+
+
+def test_conversion_missing_parameters(api_client, conversion_url):
+    data = {'from': 'USD', 'amount': '100'}
+    response = api_client.get(conversion_url, data)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+def test_conversion_negative_amount(api_client, conversion_url):
+    data = {'from': 'USD', 'to': 'UAH', 'amount': '-100'}
+    response = api_client.get(conversion_url, data)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+def test_conversion_not_supported_currency(api_client, conversion_url):
+    data = {'from': 'DKK', 'to': 'UAH', 'amount': '100'}
+    response = api_client.get(conversion_url, data)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
